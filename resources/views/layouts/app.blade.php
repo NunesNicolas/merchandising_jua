ef2fe883c011babe2cfa3760955398164f638c38
RelkzTfrog <sr.relkz01@gmail.com>
2024-03-18 10:56:14 -0300

    style:error menssage_ create user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,18 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <title>CRUD</title>
 </head>
+
 <body>
+  <div class= "container mt-2">
+    @if($errors->any())
+      @foreach ($errors->all() as $error)
+        <div class="alert alert-danger" role="alert">
+          {{$error}}
+        </div>
+      @endforeach
+    @endif
+  </div>
+
   @yield('content') 
 </body>
 </html>
