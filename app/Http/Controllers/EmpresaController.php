f8b0dba9c2bb210c98cfc5d875be5ee55a7c6b25
rubi <rubembenicio5@gmail.co>
2024-04-07 23:38:21 -0300

    Modificações inuteis

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\empresa;
+use App\Requests\EmpresaStore;
 use Illuminate\Http\Request;
 
 class EmpresaController extends Controller
@@ -10,4 +12,40 @@ class EmpresaController extends Controller
     public function empresas(){
         return view('forms.empresas');
     }
+    public function home(){
+        $users = empresa::all();
+        return view('forms.home', ['users'=>$users]);
+        }
+        
+        public function create(){
+            return view('crud.create');
+        }
+        
+        public function store($request){
+            empresa::create($request->all());
+            return redirect()->route('Usuario-home');
+        }
+        public function edit($id){
+            $users = empresa::where('id',$id)->first();
+            if(!empty($users)){
+                return view('crud.edit', ['users'=>$users]);
+            }
+            else{
+                return redirect()->route('Usuario-home');
+            }
+        }
+        public function update($request, $id)
+        {
+            $date = [
+                'nome' => $request->nome,
+                'email' => $request->email,
+                'telefone' => $request->telefone,
+             ];
+            Empresa::where('id',$id)->update($date);
+            return redirect()->route('Usuario-home');
+        }
+        public function destroy($id){
+            Empresa::where('id',$id)->delete();
+            return redirect()->route('Usuario-home');
+        }
 }
--- resources/views/forms/empresas.blade.php
+++ resources/views/forms/empresas.blade.php
@@ -1,36 +1,44 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<body class="bg-dark p-5 d-flex justify-center">
-  <form action="{{route('Empresas-form')}}" class="row text-light bg-dark p-1 border-1 justify-center w-75 border boder-light rounded" method="POST">
-     <div class="form-group form-check"> 
-      <label for="exampleInputEmail1" class="text-ligth">Nome da empresa:</label>
-      <input name="nome" type="text" class="form-control text-light bg-secondary w-50 p-1" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nome da empresa">
-    </div>
-    <div class="form-group form-check">  
-      <label for="exampleInputPassword1">Id da empresa</label>
-      <input name="id" type="text" class="form-control text-light bg-secondary w-50 p-1" id="exampleInputPassword1" placeholder="Id da empresa">
-    </div>
-    <div class="form-group form-check">
-      <label for="exampleInputPassword1">CNPJ</label>
-      <input name="cnpj" type="text" class="form-control text-light bg-secondary w-50 p-1" id="exampleInputPassword1" placeholder="CNPJ">
-    </div>
-    <div class="form-group form-check">
-      <label for="exampleInputPassword1">Latitude</label>
-      <input name="latitude" type="text" class="form-control text-light bg-secondary w-50 p-1" id="exampleInputPassword1" placeholder="latitude">
-    </div>
-    <div class="form-group form-check">
-      <label for="exampleInputPassword1">Longitude</label>
-      <input name="longitude"  type="text" class="form-control text-light bg-secondary w-50 p-1" id="exampleInputPassword1" placeholder="longitude">
-    </div>
-    <div class="form-group form-check">
-      <label for="exampleInputTextarea">Descrição</label>
-      <input class="form-control text-light bg-secondary w-50 p-1 h-25" id="exampleFormControlTextarea1" rows="3">
-    </div>
-    <div class="form-group form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Confirmar envio</label>
-    </div>
-    <button type="submit" class="btn btn-primary p-0" style="width: 80px;">Enviar</button>
-  </form>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+@extends('layouts.app')
+
+
+<title> Promotor </title>
+
+<h1 class="text-center"> Questionário do Promotor </h1>
+
+
+<form action="{{ route('Empresas-forms') }}" method="POST">
+            @csrf
+                <div class ="form-group">
+                    <label for="nome">Nome da empresa:</label>
+                    <input type="text" class ="form-control" name="nome" placeholder="Nome">
+                </div>
+                <br>
+                <div class ="form-group">
+                    <label for="email">CNPJ:</label>
+                    <input type="text" class ="form-control" name="email" placeholder="CNPJ">
+                </div>
+                <br>
+                <div class ="form-group">
+                    <label for="telefone">ID:</label>
+                    <input type="text" class ="form-control" name="telefone" placeholder="digite um Telefone">
+                </div>
+                <div class ="form-group">
+                    <label for="status">Descrição:</label>
+                    <input type="text" class ="form-control" name="status" placeholder="digite um Status">
+                </div>
+                 <div class ="form-group">
+                    <label for="status">Latitude:</label>
+                    <input type="text" class ="form-control" name="status" placeholder="digite um Status">
+                </div>
+                 <div class ="form-group">
+                    <label for="status">Longitude:</label>
+                    <input type="text" class ="form-control" name="status" placeholder="digite um Status">
+                </div>
+                <br>
+                <br>
+                <div class ="form-group">
+                    <input type="submit" nome="submit" class="btn btn-primary">
+                </div>
+
+            </form>
+  </div>
