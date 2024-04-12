e4a239504bd9fea07a7abe0cfbc4bf77b4960b82
RelkzTfrog <sr.relkz01@gmail.com>
2024-04-12 08:54:28 -0300

    promotores table

---
+++ app/Http/Controllers/PromotoresController.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Support\MessageBag;
+use App\Http\Requests\UsuarioStore;
+
+class UsuarioController extends Controller
+{
+    // public function home(){
+    // $users = Usuario::all();
+    // return view('crud.home', ['users'=>$users]);
+    // }
+    
+    // public function create(){
+    //     return view('crud.create');
+    // }
+    
+    // public function store(UsuarioStore $request){
+    //     Usuario::create($request->all());
+    //     return redirect()->route('Usuario-home');
+    // }
+    // public function edit($id){
+    //     $users = Usuario::where('id',$id)->first();
+    //     if(!empty($users)){
+    //         return view('crud.edit', ['users'=>$users]);
+    //     }
+    //     else{
+    //         return redirect()->route('Usuario-home');
+    //     }
+    // }
+    public function update(PromotoreStore $request, $id)
+    {
+        $date = [
+            'nome' => $request->nome,
+            'email' => $request->email,
+            'senha' => bcrypt($request->senha),  
+            'telefone' => $request->telefone,
+         ];
+        Promotore::where('id',$id)->update($date);
+        return redirect()->route(/*'Usuario-home'*/);
+    }
+    public function destroy($id){
+        Promotore::where('id',$id)->delete();
+        return redirect()->route(/*'Usuario-home'*/);
+    }
+    
+}
