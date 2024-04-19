30c82e6bb4e79521e8946c445c94488b38fdf92e
RelkzTfrog <sr.relkz01@gmail.com>
2024-04-19 13:19:56 -0300

    request_produtos

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -4,43 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Models\Produto;
 use Illuminate\Http\Request;
+use  App\Requests\ProdutoStore;
 
 class ProdutosController extends Controller
 {
-    public function index()
-    {
-        $produtos = Produto::all();
-        return view('produtos.index', ['produtos'=>$produtos]);
-    }
-    public function create()
-    {
-        return view('produtos.createprodutos');
-    }
-    public function store(Request $request)
-    {
-        Produto::create($request->all());
-        return redirect()->route('Forms-index');
-    }
-    public function edit($id)
-    {
-        $produtos = Produto::where('id', $id)->first();
-        if(!empty($produtos))
-        {
-            return view('produtos.editprodutos', ['produtos'=>$produtos]);        
-        }
-        else
-        {
-            return redirect()->route('Forms-index');
-        }
-    }
+    // public function index()
+    // {
+    //     $produtos = Produto::all();
+    //     return view('produtos.index', ['produtos'=>$produtos]);
+    // }
+    // public function create()
+    // {
+    //     return view('produtos.createprodutos');
+    // }
+    // public function store(Request $request)
+    // {
+    //     Produto::create($request->all());
+    //     return redirect()->route('Forms-index');
+    // }
+    // public function edit($id)
+    // {
+    //     $produtos = Produto::where('id', $id)->first();
+    //     if(!empty($produtos))
+    //     {
+    //         return view('produtos.editprodutos', ['produtos'=>$produtos]);        
+    //     }
+    //     else
+    //     {
+    //         return redirect()->route('Forms-index');
+    //     }
+    // }
     public function update(Request $request, $id)
     {
         $data = [
-            'nomeproduto' => $request->nomeproduto,
-            'precoproduto' => $request->precoproduto,
-            'marcaproduto' => $request->marcaproduto,
-            'tipoproduto' => $request->tipoproduto,
-            'numerovendas' => $request->numerovendas,
+            'nome' => $request->nome,
+            'img' => $request->email,
+            'weight' => $request->weight,  
 
         ];
         Produto::where('id', $id)->update($data);
