04e5397db24a3525e6aa6bfc00f52701bbafd248
rubi <rubembenicio5@gmail.co>
2024-04-21 07:00:22 -0300

    tudo ajeitado, falta o botão azul adicionem.

--- resources/views/produtos/produtoinfo.blade.php
+++ resources/views/produtos/produtoinfo.blade.php
@@ -43,8 +43,8 @@
     border-bottom: #2C9AFF;
   }
   .addButton{
-    width: 4vw;
-    height: 7vh;
+    width: 2.5vw;
+    height: 5vh;
     background-color: transparent;
     border-radius: 0.5vh;
     border: 0.1vh solid #858585;
@@ -53,6 +53,12 @@
   .addButton:hover{
     border: 0.1vh solid #2C9AFF;
     color: #2C9AFF;
+  }
+  a:visited {
+    color: purple;
+  }
+  .miniBt{
+    cursor: pointer;
   }
 </style>
 <section style="
@@ -84,12 +90,25 @@
     <div class="d-flex" style="
       border: 0.2vh solid #000;
       width: 85vw;
-      height: 43vh;
+      height: 50vh;
       border-radius: 1vh;
       flex-wrap: wrap;
       justify-content: space-evenly;
     ">
-        <img src="" style="width: 15vw; height: 29vh; margin-top: 1vh;">
+      <div style="width: 100%; height: 5vh; justify-content: end; display: flex; padding: 0.5vh;">
+        <a class="miniBt" style="color: red; margin-right: 0.5vw;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
+          </svg>
+        </a>
+        <a class="miniBt" style="color: #2C9AFF;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+          </svg>
+        </a>
+      </div>
+        <img src="" style="width: 15vw; height: 29vh;">
         <div style="display: flex; width: 68vw; height: 30vh; flex-wrap: wrap; align-items: center; justify-content: space-between; padding: 1vh;">
           <label>Nome do Produto</label>
           <p class="inputFachada" style="width: 67vw;"> Produto</p>
@@ -114,10 +133,11 @@
         </div>
     </div>
    </div>
-    <></>
     <div class="d-flex" style="width: 100%; height: 5vh; justify-content:end; padding:20px">
       <p class="mr-auto" style=" margin-left: 3vw; color:#858585; font-weight: bold;"> Sem produtos concorrentes cadastrados  </p>
     </div>
   </section>
+
 </main>
+
 @endsection
