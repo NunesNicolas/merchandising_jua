a0beca227bdad5307d224b9b1cfe2478492ba2b4
RelkzTfrog <sr.relkz01@gmail.com>
2024-05-02 10:31:19 -0300

    feat:empresas-info

--- resources/views/empresa/home.blade.php
+++ resources/views/empresa/home.blade.php
@@ -3,8 +3,11 @@
 @section('title','criação')
 
     @section('content')
+    <div class="d-flex" style="width: 100%; height: 5vh; justify-content:space-between; padding:10px">
+      <p class="ml-4 mr-auto" style="color:#858585; font-weight: bold; font-size: 2.2vh;" > Clientes   </p>
+      <p class="mr-3" style="color:#858585; font-weight: bold; font-size: 2.2vh;"> Dashboard / Clientes  </p>
+    </div>
 
-
-
+    
 </main>
     @endsection
