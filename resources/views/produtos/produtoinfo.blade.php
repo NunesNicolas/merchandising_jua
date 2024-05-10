6e09d9b8315f3fd015c1f4bc11481e23872fe342
JocielioSoares <jocieliosoares2005@gmail.com>
2024-05-10 13:54:06 -0300

    table competitors

--- resources/views/produtos/produtoinfo.blade.php
+++ resources/views/produtos/produtoinfo.blade.php
@@ -141,9 +141,55 @@
 
     {{-- FALTA FAZER O IF ELSE ENTRE ESSAS 2 DIVS --}}
 
-    <div class="d-flex" style="width: 100%; height: 5vh; justify-content:end; padding:20px">
+    {{-- <div class="d-flex" style="width: 100%; height: 5vh; justify-content:end; padding:20px">
       <p class="mr-auto" style=" margin-left: 3vw; color:#858585; font-weight: bold;"> SEM PRODUTOS CONCORRENTES CADASTRADOS </p>
+    </div> --}}
+
+    {{--------------------------------------------------------------------------------}}
+
+    <div class="d-flex" style="width: 100%; height: 10vh; display:flex; ">
+      <p class="d-flex" style=" margin-left: 4.5vw; font-weight: bold; display: flex"> 
+        <table class="" style="width: 50%; background-color:; text-align:center">
+          
+            <th class="" style="width:">PRODUTOS</th>
+            <th style="width:">CONCORRENTES</th>
+          
+        </table>  
+      </p>
     </div>
+
+    <div class="d-flex" style="
+    width: 90%;
+    height:10vh;
+    display: flex;
+    justify-contente;
+    border: none;
+    align-items: center;
+    background-color:#ffffff;
+    border-radius:15px;
+    ">
+
+      
+          <table class="" style="text-align: center; color:#858585; width:100% ">
+            
+            <th style="width: 20% ; border-radius:15px 0 0 15px;">Desinfetante</th>
+            <th style="width: 20%">Ypê</th>
+            
+            <th style="width:20%">Editar produto <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+            </svg> </th>
+            
+            <th style="width:20% ; border-radius:0 15px 15px 0;">Detalhes <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+              <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
+              <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+            </svg> </th>
+            
+          </table>
+        </p>
+    </div>
+    {{--------------------------------------------------------------------------------}}
+    
     
   </section>
 </main>
