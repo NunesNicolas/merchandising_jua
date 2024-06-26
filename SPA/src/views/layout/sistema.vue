ddaf59a18964244e78ad680b8846b56810b9d3a1
Domingos <domingos.adj@gmail.com>
2024-06-26 17:23:11 -0300

    changes

--- SPA/src/views/layout/sistema.vue
+++ SPA/src/views/layout/sistema.vue
@@ -1,77 +1,49 @@
 <script setup>
-import navbar from '../../components/header/navbar.vue'
-import axios from "axios";
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+import Navbar from '../../components/header/navbar.vue'; // Certifique-se de importar o componente Navbar corretamente
 
-import {ref, onMounted} from 'vue'
+const user = ref(null);
 
-const user = ref();
-onMounted(async ()=> {
-    // const data = await axios.get('http://127.0.0.1:8000/api/user');
-    console.log(data);
-})
+onMounted(async () => {
+  
+  
+
+  try {
+    //@todo verificar depois
+    // const response = await axios.get('/user');
+    // user.value = response.data; // Armazena os dados do usuário na variável reativa 'user'
+    user.value = {name: "Usuário de teste"}
+  } catch (error) {
+    console.error('Erro ao obter dados do usuário:', error);
+  }
+});
 </script>
 
 <template>
+  <div>
+    <Navbar :user="user" /> <!-- Passe o objeto 'user' para o componente Navbar -->
 
-    <navbar :user="promotor" />  
-      
     <main id="topheader">
       <nav class="navbar bg-body-tertiary" style="background-color: #ffffff; height: 8vh; width: 100%">
-        <a class="ml-auto" style="">
+        <a class="ml-auto">
           <img src="https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Pic.png" alt="Perfil" style="width:2vw; height: 4vh;">
-          Olá, {{promotor.nome}}!
+          <!-- Exibe o nome do usuário se estiver definido -->
+          <template v-if="user">
+            Olá, {{ user.name }}!
+          </template>
         </a>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
           <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
         </svg>
       </nav>
       <router-view :key="$route.fullPath" />
-      
     </main>
+  </div>
 </template>
 
-<script>
+<!-- Remova a tag <script> do componente, pois o código está dentro de <script setup> -->
 
-export default {
-  data(){
-    return{
-      promotor: {
-        id: '',
-        nome: ''
-      }
-    } 
-    },
-
-  methods: {
-      // async loginVerificar(){
-      //   let formData = new FormData();
-      //   formData.append('idTransition', this.$route.params.userid);
-
-      //   try {
-      //     let response = await axios.post('http://127.0.0.1:8000/api/transition', formData);
-      //     this.promotor.id = response.data.userid
-      //     this.promotor.nome = response.data.usernome
-      //   }catch (error) {
-      //     console.error('Erro:', error);
-      //     if (error.response && error.response.data && error.response.data.message) {
-      //         alert(error.response.data.message);
-      //         this.$router.push({name:'login'});
-      //     } else {
-      //         alert('Ocorreu um erro ao fazer o login.');
-      //     }
-      //   }
-        
-
-      // }
-    },
-
-  mounted() { 
-    // this.loginVerificar();
-  }
-
-  
-}
-</script>
-
-<style>
+<style scoped>
+/* Estilos específicos do componente, se necessário */
 </style>
