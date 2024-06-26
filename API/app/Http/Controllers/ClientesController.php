2dd7d2e38b59b536ebeb985325d52648eb21fedf
Domingos <domingos.adj@gmail.com>
2024-06-26 18:00:12 -0300

    changes

---
+++ API/app/Http/Controllers/ClientesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cliente;
+
+class ClientesController extends Controller
+{
+    public function index()
+    {
+        $clientes = Cliente::all();
+        return response()->json(['data' => $clientes]);
+    }
+
+    public function store(Request $request)
+    {
+        $cliente = Cliente::create($request->all());
+        return response()->json(['data' => $cliente], 201);
+    }
+
+    public function show(Cliente $cliente)
+    {
+        return response()->json(['data' => $cliente]);
+    }
+
+    public function update(Request $request, Cliente $cliente)
+    {
+        $cliente->update($request->all());
+        return response()->json(['data' => $cliente]);
+    }
+
+    public function destroy(Cliente $cliente)
+    {
+        $cliente->delete();
+        return response()->json(null, 204);
+    }
+}
--- API/routes/api.php
+++ API/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,32 +23,36 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('produtos')->group (function(){
-    Route::get('/', [ProdutosController::class , 'home'])->name('Produtos-home');
-    Route::get('/{id?}', [ProdutosController::class , 'info'])->name('Produtos-info');
-    Route::post('save_produto', [ProdutosController:: class, 'saveProduto']);
-    Route::put('/{id?}', [ProdutosController::class , 'update'])->where('id', '[0-9]+');
-    }); 
-    
-    Route::prefix('competitors')->group (function(){
-        Route::get('/create', [ProdutosController::class , 'create_competitors'])->name('Competitors-create');
-        Route::post('/', [ProdutosController::class , 'store_competitors'])->name('Competitors-store');  
-    });
-    
-    Route::prefix('promotores')->group(function(){
-        Route::get('/', [PromotoresController::class , 'home'])->name('Promotores-home');
-        Route::get('/create', [PromotoresController::class , 'create'])->name('Promotores-create');
-        Route::post('/', [PromotoresController::class , 'store'])->name('Promotores-store');
-        Route::get('/{id?}', [PromotoresController::class , 'info'])->name('Promotores-info');
-        Route::post('save_promotor', [PromotoresController:: class, 'savePromotor']);
-    });
-    
-    Route::prefix('empresas')->group(function(){
-        Route::get('/', [EmpresasController::class , 'index'])->name('Empresas-index');
-        Route::post('/', [EmpresasController::class , 'store'])->name('Empresas-store');
-        Route::get('/{id}', [EmpresasController::class , 'info'])->where('id','[0-9]')->name('Empresa-info');
-        Route::put('/{id}', [EmpresasController::class , 'update'])->where('id','[0-9]')->name('Empresas-update');
-        Route::delete('/{id}', [EmpresasController::class, 'destroy'])-> where('id', '[0-9]') -> name('Empresas-destroy');
-        Route::post('save_empresa', [EmpresasController::class, 'saveEmpresa']);
-    });
-    
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [ProdutosController::class, 'home'])->name('Produtos-home');
+    Route::get('/{id?}', [ProdutosController::class, 'info'])->name('Produtos-info');
+    Route::post('save_produto', [ProdutosController::class, 'saveProduto']);
+    Route::put('/{id?}', [ProdutosController::class, 'update'])->where('id', '[0-9]+');
+});
+
+Route::prefix('competitors')->group(function () {
+    Route::get('/create', [ProdutosController::class, 'create_competitors'])->name('Competitors-create');
+    Route::post('/', [ProdutosController::class, 'store_competitors'])->name('Competitors-store');
+});
+
+Route::prefix('promotores')->group(function () {
+    Route::get('/', [PromotoresController::class, 'home'])->name('Promotores-home');
+    Route::get('/create', [PromotoresController::class, 'create'])->name('Promotores-create');
+    Route::post('/', [PromotoresController::class, 'store'])->name('Promotores-store');
+    Route::get('/{id?}', [PromotoresController::class, 'info'])->name('Promotores-info');
+    Route::post('save_promotor', [PromotoresController::class, 'savePromotor']);
+});
+
+Route::prefix('empresas')->group(function () {
+    Route::get('/', [EmpresasController::class, 'index'])->name('Empresas-index');
+    Route::post('/', [EmpresasController::class, 'store'])->name('Empresas-store');
+    Route::get('/{id}', [EmpresasController::class, 'info'])->where('id', '[0-9]')->name('Empresa-info');
+    Route::put('/{id}', [EmpresasController::class, 'update'])->where('id', '[0-9]')->name('Empresas-update');
+    Route::delete('/{id}', [EmpresasController::class, 'destroy'])->where('id', '[0-9]')->name('Empresas-destroy');
+    Route::post('save_empresa', [EmpresasController::class, 'saveEmpresa']);
+});
+
+Route::prefix('clientes')->group(function () {
+    Route::get('/', [ClientesController::class, 'index'])->name('clientes-home');
+    Route::post('/', [ClientesController::class, 'store'])->name('clientes-store');
+});
