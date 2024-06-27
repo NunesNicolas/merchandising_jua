c70cf0476b002d314dd2232acec2093c8fe72cc0
Domingos <domingos.adj@gmail.com>
2024-06-27 16:52:56 -0300

    fix: issue on login

--- SPA/src/views/Auth/inicial.vue
+++ SPA/src/views/Auth/inicial.vue
@@ -9,16 +9,16 @@
         <h2 class="text-center">Bem-vindo ao sistema</h2>
 
         <div class="input-container">
-          <input type="email" v-model="form.email" required placeholder="Email" />
+          <input type="email" v-model="email" required placeholder="Email" />
         </div>
 
         <div class="input-container">
-          <input type="password" v-model="form.senha" required placeholder="Senha" />
+          <input type="password" v-model="senha" required placeholder="Senha" />
           <i class="bi bi-eye-fill" @click="mostrarSenha"></i>
         </div>
 
         <div class="form-link">
-          <router-link to="/esqueceu-senha">Esqueceu sua senha?</router-link>
+          <router-link to="#">Esqueceu sua senha?</router-link>
         </div>
 
         <button type="submit" class="btn-login">
@@ -29,67 +29,71 @@
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
+
+<script>
 import axios from 'axios';
 import { useRouter } from 'vue-router';
 
-const form = ref({
-  email: '',
-  senha: '',
-});
+export default {
+  data() {
+    return {
+      email: '',
+      senha: '',
+    };
+  },
+  methods: {
+    mostrarSenha() {
+      // Lógica para mostrar a senha
+    },
+    async login() {
+      try {
+        const response = await axios.get('http://localhost:8000/sanctum/csrf-cookie');
+        const csrfToken = this.getTokenFromCookie();
 
-const router = useRouter();
+        const formData = new FormData();
+        formData.append('email', this.email);
+        formData.append('password', this.senha);
 
-const mostrarSenha = () => {
-  // Lógica para mostrar a senha
-};
+        const loginResponse = await axios.post('http://localhost:8000/login', formData, {
+          headers: {
+            Accept: 'application/json',
+            'X-CSRF-TOKEN': csrfToken,
+          },
+        });
 
-const login = async () => {
-  try {
-    const response = await axios.get('http://localhost:8000/sanctum/csrf-cookie');
-    const csrfToken = getTokenFromCookie();
+        localStorage.setItem('token', csrfToken);
 
-    const formData = new FormData();
-    formData.append('email', form.email);
-    formData.append('password', form.senha);
+        console.log('Login successful:', loginResponse.data);
 
-    const loginResponse = await axios.post('http://localhost:8000/login', formData, {
-      headers: {
-        Accept: 'application/json',
-        Referer: 'localhost:8000/',
-        'X-CSRF-TOKEN': csrfToken,
-      },
-    });
+        this.$router.push('/'); // Redirecionar após o login
 
-    // Armazenar o token em localStorage
-    localStorage.setItem('token', csrfToken);
-
-    console.log('Login successful:', loginResponse.data);
-
-    // Redirecionar para a página após o login
-    router.push('/');
-  } catch (error) {
-    console.error('Erro ao fazer login:', error);
-    if (error.response && error.response.data && error.response.data.message) {
-      console.log('Error message:', error.response.data.message);
-    } else {
-      console.log('Ocorreu um erro ao fazer o login.');
-    }
-  }
-};
-
-const getTokenFromCookie = () => {
-  let csrfToken = '';
-  const cookies = document.cookie.split('; ');
-  cookies.forEach((cookie) => {
-    if (cookie.startsWith('XSRF-TOKEN=')) {
-      csrfToken = decodeURIComponent(cookie.split('=')[1]);
-    }
-  });
-  return csrfToken;
+      } catch (error) {
+        console.error('Erro ao fazer login:', error);
+        if (error.response && error.response.data && error.response.data.message) {
+          console.log('Error message:', error.response.data.message);
+        } else {
+          console.log('Ocorreu um erro ao fazer o login.');
+        }
+      }
+    },
+    getTokenFromCookie() {
+      let csrfToken = '';
+      const cookies = document.cookie.split('; ');
+      cookies.forEach((cookie) => {
+        if (cookie.startsWith('XSRF-TOKEN=')) {
+          csrfToken = decodeURIComponent(cookie.split('=')[1]);
+        }
+      });
+      return csrfToken;
+    },
+  },
 };
 </script>
+
+<style scoped>
+/* Estilos CSS permanecem os mesmos */
+</style>
+
 
 <style scoped>
 .login-container {
