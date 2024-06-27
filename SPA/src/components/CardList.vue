e79cda8bda24348715743b672683c7f5136e1c9a
Domingos <domingos.adj@gmail.com>
2024-06-27 13:28:36 -0300

    empty cardlist

--- SPA/src/components/CardList.vue
+++ SPA/src/components/CardList.vue
@@ -1,33 +1,44 @@
 <template>
     <div>
-        <div v-for="item in items" :key="item.id" class="d-flex">
+        <div v-if="items.length === 0" class="no-data-message">
             <div class="d-flex flex-wrap content-normal bg-light ml-4" id="infos" style="">
                 <table style="width: 80%;">
-                    <thead>
-                        <tr class="indicadores">
-                            <th v-for="(label, field) in fields" :key="field">{{ label }}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td v-for="(label, field) in fields" :key="field">{{ item[field] }}</td>
-                        </tr>
-                    </tbody>
+                    <p>Não há dados disponíveis.</p>
                 </table>
-                <router-link :to="'empresas/' + item.id" class="d-flex flex-wrap">Ver atividade 
-                    <svg xmlns="http://www.w3.org/2000/svg" style="width: 2vw; height: 4vh;" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
-                        <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
-                        <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
-                    </svg>
-                </router-link>
+            </div>
+        </div>
+        <div v-else>
+            <div v-for="item in items" :key="item.id" class="d-flex">
+                <div class="d-flex flex-wrap content-normal bg-light ml-4" id="infos" style="">
+                    <table style="width: 80%;">
+                        <thead>
+                            <tr class="indicadores">
+                                <th v-for="(label, field) in fields" :key="field">{{ label }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td v-for="(label, field) in fields" :key="field">{{ item[field] }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <router-link :to="'empresas/' + item.id" class="d-flex flex-wrap">Ver atividade
+                        <svg xmlns="http://www.w3.org/2000/svg" style="width: 2vw; height: 4vh;" fill="currentColor"
+                            class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+                            <path
+                                d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5" />
+                            <path
+                                d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+                        </svg>
+                    </router-link>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-<script>
-import axios from 'axios';
 
+<script>
 export default {
     props: {
         items: {
