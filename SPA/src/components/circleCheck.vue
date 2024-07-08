6d09926e4bd34c2be0d4aeb6150ea67d2f4a1b3f
RelkzTfrog <sr.relkz01@gmail.com>
2024-07-08 12:12:50 -0300

    fix

--- SPA/src/components/circleCheck.vue
+++ SPA/src/components/circleCheck.vue
@@ -1,71 +1,92 @@
 <template>
     <div class="container">
-        <div v-for="(camp, field) in defaults" class="round">
-            <input type="checkbox" :id="field" :value="camp[label]" :checked="checkin(camp[label])"
-                @change="update(camp, field)" />
-            <label :for="field" class="circle">
-                {{ camp[label] }}
-            </label>
-        </div>
+      <div v-for="(camp, field) in defaults" :key="field" class="round">
+        <input
+          type="checkbox"
+          :id="field"
+          :value="camp[label]"
+          :checked="checkin(camp[label])"
+          @change="update(camp, field)"
+        />
+        <label :for="field" class="circle">
+          {{ camp[label] }}
+        </label>
+      </div>
     </div>
-</template>
-
-<script>
-export default {
+    <slot>
+       
+    </slot>
+  </template>
+  
+  <script>
+  export default {
     props: {
-        defaults: Object,
-        options: Array,
-        instance: Object,
-        value: '',
-        label: '',
+      defaults: Object,
+      options: Array,
+      instance: Object,
+      value: String,
+      label: String,
+      finalize: Function,
     },
     data() {
-        return {
-            selectedOptions: [],
-        };
+      return {
+        selectedOptions: [],
+      };
     },
     created() {
-        this.selectedOptions = this.options.map(option => option[this.label]);
+      this.initializeSelectedOptions();
+    },
+    watch: {
+      options: {
+        handler(newOptions) {
+          this.initializeSelectedOptions();
+        },
+        deep: true,
+        immediate: true,
+      },
     },
     methods: {
-        checkin(object) {
-            return this.options.some(option => object == option[this.label]);
-        },
-
-        update(camp, field) {
-            const value = camp[this.label];
-            const index = this.selectedOptions.findIndex(option => option === value);
-
-            if (index === -1) {
-                this.selectedOptions.push(value);
-            } else {
-                this.selectedOptions.splice(index, 1);
-            }
-
-            console.log(this.selectedOptions)
-        },
+      initializeSelectedOptions() {
+        this.selectedOptions = this.options.map(option => option[this.label]);
+      },
+      checkin(object) {
+        return this.selectedOptions.includes(object);
+      },
+      update(camp, field) {
+        const value = camp[this.label];
+        const index = this.selectedOptions.findIndex(option => option === value);
+  
+        if (index === -1) {
+          this.selectedOptions.push(value);
+        } else {
+          this.selectedOptions.splice(index, 1);
+        }
+  
+        console.log(this.selectedOptions);
+      },
     },
-};
-</script>
-
-<style scoped>
-.container {
+  };
+  </script>
+  
+  <style scoped>
+  .container {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-}
-
-.round {
+  }
+  
+  .round {
     display: flex;
     align-items: center;
-}
-
-input {
+  }
+  
+  input {
     height: 40px;
-}
-
-.selected {
+  }
+  
+  .selected {
     font-weight: bold;
     color: #666;
-}
-</style>
+  }
+  </style>
+  
--- SPA/src/views/produtos/Show.vue
+++ SPA/src/views/produtos/Show.vue
@@ -1,113 +1,119 @@
 <template>
-<div>
-  <Breadcrumb pageTitle="Produtos" routeInfo="Dashboard / Produtos" />
+    <div>
+        <Breadcrumb pageTitle="Produtos" routeInfo="Dashboard / Produtos" />
 
-  <ActionListWrapper>
-    <ActionRouterBack />
-  </ActionListWrapper>
+        <ActionListWrapper>
+            <ActionRouterBack />
+        </ActionListWrapper>
 
-  <div class="page" id="BodyAll" ref="BodyAll">
-    <BoxInfoWrapper>
-    <slot>
-        <div class="d-flex">
-            <div class="box-line">
-                <img :src="produto.img" height="150px" width="auto" />
-            </div>
-            <div style="width: 100%;">
-                <div class="box-line">
-                    <BoxInfo title="Nome" :value="produto.nome"></BoxInfo>
-                </div>
-                <div class="d-flex" style="width: 100%; gap:10px; justify-content:space-between">
-                    <div class="box-line" style="width: 50%">
-                        <BoxInfo title="Preço médio:"></BoxInfo>
+        <div class="page" id="BodyAll" ref="BodyAll">
+            <BoxInfoWrapper>
+                <slot>
+                    <div class="d-flex">
+                        <div class="box-line">
+                            <img :src="produto.img" height="150px" width="auto" />
+                        </div>
+                        <div style="width: 100%;">
+                            <div class="box-line">
+                                <BoxInfo title="Nome" :value="produto.nome"></BoxInfo>
+                            </div>
+                            <div class="d-flex" style="width: 100%; gap:10px; justify-content:space-between">
+                                <div class="box-line" style="width: 50%">
+                                    <BoxInfo title="Preço médio:"></BoxInfo>
+                                </div>
+                                <div class="box-line" style="width: 50%">
+                                    <BoxInfo title="Quantidade de concorrentes:" :value="competitors.length"></BoxInfo>
+                                </div>
+                            </div>
+                        </div>
+                        <DeleteBT :item="produto" :label="'nome'" :url="'/produtos/'" />
+                        <router-link :to="{
+                            name: 'UpdateProdutos',
+                            params: { id: this.$route.params.id },
+                        }">
+                            <i class="bi bi-pencil-square ml-2" style="font-size: 17px; color:dodgerblue"></i>
+                        </router-link>
                     </div>
-                    <div class="box-line" style="width: 50%">
-                        <BoxInfo title="Quantidade de concorrentes:" :value="competitors.length"></BoxInfo>
+                    <div class="divFooter" style="width: 100%">
+
+                        <FooterButtons :id="id" :label="'weight'" :instance="produto" :items="variants"
+                            :defaults="weights" />
+
+                        <b-button v-b-modal.modal-1 class="" style="padding: 5px; background-color: white; margin: 2px">
+                            <i class="bi bi-plus-circle" style="font-size:30px; color: black; border: none"></i>
+                        </b-button>
+                        <b-modal id="modal-1" title="Editar Tamanho do Produto">
+                            <CheckBox :options="variants" :value="'id'" :label="'weight'" :instance="produto"
+                                :defaults="weights" />
+                            <slot>
+                                <button>Cancelar</button>
+                                <button @click="updateweights(selectedOptions)">Atualizar</button>
+                            </slot>
+                        </b-modal>
                     </div>
-                </div>
+
+                </slot>
+            </BoxInfoWrapper>
+
+            <div class="btCompetitorAdd">
+                <router-link :to="{
+                    name: 'CreateCompetitors',
+                    params: { id: this.$route.params.id },
+                }">
+                    <h5 style="color: white; text-align:center">Adicionar Concorrente</h5>
+                </router-link>
             </div>
-            <DeleteBT :item="produto" :label="'nome'" :url="'/produtos/'"/>
-            <router-link :to="{
-                name: 'UpdateProdutos',
-                params: { id: this.$route.params.id },
+
+            <CardList :items="competitors" :fields="{
+                nome: 'PRODUTOS',
+                brand: 'CONCORRENTES',
             }">
-                <i class="bi bi-pencil-square ml-2" style="font-size: 17px; color:dodgerblue"></i>
-            </router-link>
+                <template v-slot:actions="{ item }">
+                    <router-link :to="{
+                        name: 'UpdateCompetitors',
+                        params: {
+                            id: this.$route.params.id,
+                            compid: item.id,
+                        },
+                    }" class="d-flex flex-wrap">
+                        <i class="bi bi-pencil-square" style="font-size: 2rem; color: grey"></i>
+                    </router-link>
+
+                    <button @click="toggleModal(item.id)" class="buttonComp">
+                        <i class="bi bi-file-earmark-text" style="font-size: 2rem; color: grey"></i>
+                    </button>
+
+                    <DetalhesModal @modificarEstilo="modificarEstilo" @toggleModal="toggleModal()"
+                        v-show="modalEdit == item.id" :title="'DETALHES PRODUTO CONCORRENTE'" :value="modalEdit"
+                        :item="item" :fields="{
+                            nome: 'Nome',
+                            brand: 'Marca',
+                        }">
+                        <slot>
+                            <div>
+                                <h5 class="subtitle">Preço nos nossos clientes</h5>
+                                <div class="linha-horizontal"></div>
+                            </div>
+                            <div class="divtable">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>CLIENTES</th>
+                                            <th>JUÁ</th>
+                                            <th>MARCA</th>
+                                        </tr>
+                                        <td>PEDRO LUCAS MOREIRA</td>
+                                        <td>R$ 15,00</td>
+                                        <td>R$ 15,99</td>
+                                    </thead>
+                                </table>
+                            </div>
+                        </slot>
+                    </DetalhesModal>
+                </template>
+            </CardList>
         </div>
-                <div class="divFooter" style="width: 100%">
-
-                    <FooterButtons :id="id" :label="'weight'" :instance="produto" :items="variants" :defaults="weights" />
-
-                    <b-button v-b-modal.modal-1 class="" style="padding: 5px; background-color: white; margin: 2px">
-                        <i class="bi bi-plus-circle" style="font-size:30px; color: black; border: none"></i>
-                    </b-button>
-                    <b-modal id="modal-1" title="Editar Tamanho do Produto">
-                    <CheckBox :options="variants" :value="'id'" :label="'weight'" :instance="produto" 
-                    :defaults="weights" />
-                    </b-modal>
-                </div>
-
-            </slot>
-        </BoxInfoWrapper>
-
-        <div class="btCompetitorAdd">
-            <router-link :to="{
-                name: 'CreateCompetitors',
-                params: { id: this.$route.params.id },
-            }">
-                <h5 style="color: white; text-align:center">Adicionar Concorrente</h5>
-            </router-link>
-        </div>
-
-    <CardList :items="competitors" :fields="{
-      nome: 'PRODUTOS',
-      brand: 'CONCORRENTES',
-    }">
-      <template v-slot:actions="{ item }">
-        <router-link :to="{
-          name: 'UpdateCompetitors',
-          params: {
-            id: this.$route.params.id,
-            compid: item.id,
-          },
-        }" class="d-flex flex-wrap">
-          <i class="bi bi-pencil-square" style="font-size: 2rem; color: grey"></i>
-        </router-link>
-
-        <button @click="toggleModal(item.id)" class="buttonComp">
-          <i class="bi bi-file-earmark-text" style="font-size: 2rem; color: grey"></i>
-        </button>
-
-        <DetalhesModal @modificarEstilo="modificarEstilo" @toggleModal="toggleModal()" v-show="modalEdit == item.id"
-          :title="'DETALHES PRODUTO CONCORRENTE'" :value="modalEdit" :item="item" :fields="{
-            nome: 'Nome',
-            brand: 'Marca',
-          }">
-          <slot>
-            <div>
-              <h5 class="subtitle">Preço nos nossos clientes</h5>
-              <div class="linha-horizontal"></div>
-            </div>
-            <div class="divtable">
-              <table>
-                <thead>
-                  <tr>
-                    <th>CLIENTES</th>
-                    <th>JUÁ</th>
-                    <th>MARCA</th>
-                  </tr>
-                  <td>PEDRO LUCAS MOREIRA</td>
-                  <td>R$ 15,00</td>
-                  <td>R$ 15,99</td>
-                </thead>
-              </table>
-            </div>
-          </slot>
-        </DetalhesModal>
-      </template>
-    </CardList>
-  </div>
-</div>
+    </div>
 </template>
 
 <script>
@@ -127,160 +133,174 @@ import CheckBox from '../../components/circleCheck.vue';
 
 
 export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-      produto: "",
-      competitors: [],
-      variants: [],
-      weights: [],
-      temCompetitors: false,
-      modalEdit: "",
-      BodyAll: "page",
-      weights: [
-                        { weight: '200g' },
-                        { weight: '500g' },
-                        { weight: '1kg' },
-                        { weight: '5kg' },
-                        { weight: '500ml' },
-                        { weight: '1l' },
-                        { weight: '2l' },
-                        { weight: '5l' },
-                    ]
-    };
-  },
-
-  methods: {
-    async getProdutos() {
-      let response = axios.get("/produtos/" + this.id);
-      this.competitors = (await response).data.competitorsthis;
-      this.variants = (await response).data.produtovariants;
-      this.produto = (await response).data.produto;
-      this.paginaAtual = this.produto.weight;
-
-      if (this.competitors[0] != null) {
-        this.temCompetitors = true;
-      }
+    data() {
+        return {
+            id: this.$route.params.id,
+            produto: "",
+            competitors: [],
+            variants: [],
+            weights: [],
+            temCompetitors: false,
+            modalEdit: "",
+            BodyAll: "page",
+            weights: [
+                { weight: '200g' },
+                { weight: '500g' },
+                { weight: '1kg' },
+                { weight: '5kg' },
+                { weight: '500ml' },
+                { weight: '1l' },
+                { weight: '2l' },
+                { weight: '5l' },
+            ]
+        };
     },
 
-    modificarEstilo() {
-      this.$refs.BodyAll.classList.toggle("pageMod");
+    methods: {
+        async getProdutos() {
+            let response = axios.get("/produtos/" + this.id);
+            this.competitors = (await response).data.competitorsthis;
+            this.variants = (await response).data.produtovariants;
+            this.produto = (await response).data.produto;
+            this.paginaAtual = this.produto.weight;
+
+            if (this.competitors[0] != null) {
+                this.temCompetitors = true;
+            }
+        },
+        toggleModal(id) {
+            const bodyAllElement = this.$refs.BodyAll;
+            if (this.modalEdit == id) {
+                this.modalEdit = null;
+                this.modificarEstilo();
+            } else if (this.modalEdit != id && !bodyAllElement.classList.contains('pageMod')) {
+                this.modalEdit = id;
+                this.modificarEstilo();
+            } else {
+                this.modalEdit = id;
+            }
+
+        },
+        modificarEstilo() {
+            this.$refs.BodyAll.classList.toggle("pageMod");
+        },
+
+        updateweights(selecteds) {
+            console.log(selecteds)
+        }
     },
-  },
 
-  components: {
-    ActionListWrapper,
-    ActionRouterBack,
-    ActionRouter,
-    BoxInfoWrapper,
-    BoxInfo,
-    Breadcrumb,
-    competitorsComponents,
-    CardList,
-    DetalhesModal,
-    FooterButtons,
-    DeleteBT,
-    CheckBox,
-  },
+    components: {
+        ActionListWrapper,
+        ActionRouterBack,
+        ActionRouter,
+        BoxInfoWrapper,
+        BoxInfo,
+        Breadcrumb,
+        competitorsComponents,
+        CardList,
+        DetalhesModal,
+        FooterButtons,
+        DeleteBT,
+        CheckBox,
+    },
 
-  mounted() {
-    this.getProdutos();
-  },
+    mounted() {
+        this.getProdutos();
+    },
 };
 </script>
 
 <style scoped>
 .divtable {
-  text-align: justify;
-  width: 100%;
+    text-align: justify;
+    width: 100%;
 }
 
 table {
-  width: 100%;
+    width: 100%;
 }
 
 .linha-horizontal {
-  height: 0px;
-  width: 100%;
+    height: 0px;
+    width: 100%;
 }
 
 .subtitle {
-  margin-bottom: -15px;
-  text-align: justify;
-  font-size: 15px;
+    margin-bottom: -15px;
+    text-align: justify;
+    font-size: 15px;
 }
 
 .pageMod {
-  width: 62%;
+    width: 62%;
 }
 
-.bi.bi-file-earmark-text{
-  display: flex;
+.bi.bi-file-earmark-text {
+    display: flex;
 }
 
 .buttonComp {
-  display: flex;
-  flex: wrap;
-  background-color: #f8f9fa;
-  height: 100%;
-  text-align: center;
-  outline: none;
+    display: flex;
+    flex: wrap;
+    background-color: #f8f9fa;
+    height: 100%;
+    text-align: center;
+    outline: none;
 }
 
 .btCompetitorAdd {
-  text-align: left;
-  margin-top: 25px;
-  margin-left: 15px;
-  border-radius: 10px;
-  background-color: #2c9aff;
-  width: 17%;
-  height: 50%;
+    text-align: left;
+    margin-top: 25px;
+    margin-left: 15px;
+    border-radius: 10px;
+    background-color: #2c9aff;
+    width: 17%;
+    height: 50%;
 }
 
 .bi.bi-pencil-square {
-  color: #2c9aff;
+    color: #2c9aff;
 }
 
 .btnPeso {
-  width: 30vw;
-  height: 11vh;
-  border: none;
-  text-align: left;
+    width: 30vw;
+    height: 11vh;
+    border: none;
+    text-align: left;
 }
 
 .peso {
-  width: 5vw;
-  height: 7vh;
-  text-align: center;
-  margin-top: 3.8vh;
-  background-color: transparent;
-  border: none;
-  color: #858585;
-  border-bottom: 0.1vh solid;
-  text-decoration: none;
+    width: 5vw;
+    height: 7vh;
+    text-align: center;
+    margin-top: 3.8vh;
+    background-color: transparent;
+    border: none;
+    color: #858585;
+    border-bottom: 0.1vh solid;
+    text-decoration: none;
 }
 
 .divFooter {
-  width: 100%;
-  margin-top: 6vh;
-  height: 9vh;
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  align-items: end;
-  border-top: 0.2vh solid #d3d6db;
-  margin-bottom: -25px;
-  margin-left: -25px;
+    width: 100%;
+    margin-top: 6vh;
+    height: 9vh;
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-items: end;
+    border-top: 0.2vh solid #d3d6db;
+    margin-bottom: -25px;
+    margin-left: -25px;
 }
 
 .botao-ativo {
-  color: #2c9aff;
+    color: #2c9aff;
 }
 
 .peso:hover {
-  color: solid #2c9aff;
-  border-bottom: solid #2c9aff;
+    color: solid #2c9aff;
+    border-bottom: solid #2c9aff;
 }
-
-
 </style>
