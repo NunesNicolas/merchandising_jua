f520af38c2c8f36f5889a363e1ee03b099837d99
JocielioSoares <jocieliosoares2005@gmail.com>
2024-07-12 19:56:53 -0300

    promotores:select

---
+++ MOBI/src/components/Form/SelectInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="form-group">
+        <label :for="name">{{ label }}</label>
+        <select class="form-control" :name="name" :value="modelValue"
+            @change="$emit('update:modelValue', $event.target.value)">
+            <option value="" disabled selected hidden>Selecione...</option>
+            <option v-for="option in options" :key="option.value" :value="option.value">
+                {{ option.text }}
+            </option>
+        </select>
+        <span v-if="!modelValue" class="text-muted">Por favor, selecione uma opção.</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectInput',
+    props: {
+        label: String,
+        name: String,
+        options: Array,
+        modelValue: String
+    }
+};
+</script>
