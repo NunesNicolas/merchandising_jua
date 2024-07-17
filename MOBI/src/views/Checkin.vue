ef8535a7a0130e43378c9d123287ebea437535b7
RelkzTfrog <sr.relkz01@gmail.com>
2024-07-17 11:54:16 -0300

    check-in vue

--- MOBI/src/views/Checkin.vue
+++ MOBI/src/views/Checkin.vue
@@ -1,14 +1,54 @@
 <template>
-    <div>
-        <button @click="RouterButton(this.$route.pesquisaid)" class="routerbutton">Checkout</button>
+    <div id="all">
+        <div class="textbox">
+            <h1 class="title">Check-in</h1>
+            <h4 class="text">está preste a iniciar seu atendimento, mas antes, precisamos que confirme sua localização
+            </h4>
+        </div>
+        <div class="d-flex" style="gap: 10px; width:100%; justify-content:center">
+        <RouterBack style="width: 40%;  font-size:18px"/>
+        <a  type="button" @click="RouterButton(this.$route.pesquisaid)" id="routerbutton" class="btn btn-light align-self-center">Iniciar</a>
+    </div>
     </div>
 </template>
 <script>
+import RouterBack from '../components/ActionRouterBack.vue'
 export default {
-  methods: {
-   RouterButton(id){
-      this.$router.push({ name: 'pesquisa', params: { pesquisaid: id} });
+    methods: {
+        RouterButton(id) {
+            this.$router.push({ name: 'pesquisa', params: { pesquisaid: id } });
+        }
+    },
+    components: {
+        RouterBack,
     }
-  },
-  }
+}
 </script>
+<style scoped>
+#all{
+    margin-top:30%;
+
+}
+#routerbutton{
+    width: 40%;
+    border-radius: 20px;
+    border:solid  #858585 0.2vh;
+    color:white;
+    background-color: #47a6ff;
+    font-weight: bold;
+    font-size:18px
+}
+.textbox {
+    padding: 20px;
+    margin-bottom: 10px;
+}
+
+.title {
+    margin-bottom: 20PX;
+    font: bold;
+}
+
+.text {
+    color: rgb(112, 112, 112);
+}
+</style>
