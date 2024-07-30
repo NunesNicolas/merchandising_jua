e3ef72956cd8bd9ab049d8f542043f3a20641a6c
Domingos <domingos.adj@gmail.com>
2024-07-30 17:48:13 -0300

    seeder

--- API/database/seeders/DatabaseSeeder.php
+++ API/database/seeders/DatabaseSeeder.php
@@ -10,27 +10,10 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Dados do usuário
-        $userData = [
-            'name' => 'Usuário de teste',
-            'email' => 'teste@teste.com',
-            'password' => bcrypt('123456'),
-        ];
-
-        // Verifica se o usuário já existe pelo email
-        $existingUser = User::where('email', $userData['email'])->first();
-
-        if ($existingUser) {
-            // Atualiza os dados do usuário existente
-            $existingUser->update($userData);
-        } else {
-            // Cria um novo usuário se não existir
-            User::factory()->create($userData);
-        }
-
         $this->call([
             PromotoresSeeder::class,
-            ClienteSeeder::class
+            ClienteSeeder::class,
+            DefaultUsersSeeder::class
         ]);
 
     }
